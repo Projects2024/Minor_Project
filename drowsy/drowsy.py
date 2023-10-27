3f8f9069f9f45855ac8f3aa2bbb75c286dfbe8b1
Projects2024 <149159699+Projects2024@users.noreply.github.com>
2023-10-27 19:20:23 +0530

    drowsy.py

--- drowsy/drowsy.py
+++ drowsy/drowsy.py
@@ -1,7 +1,8 @@
 import cv2
-import numpy as np
 import dlib
+import numpy as np
 from imutils import face_utils
+import pygame
 
 cap = cv2.VideoCapture(0)
 
@@ -11,8 +12,12 @@ predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 sleep = 0
 drowsy = 0
 active = 0
+yawn = 0
 status = ""
 color = (0, 0, 0)
+
+pygame.mixer.init()
+alarm_sound = pygame.mixer.Sound("alarm.mp3")
 
 def compute(ptA, ptB):
     dist = np.linalg.norm(ptA - ptB)
@@ -36,8 +41,8 @@ while True:
 
     faces = detector(gray)
 
-    # Initialize face_frame outside the loop
     face_frame = frame.copy()
+    alarm_playing = False
 
     for face in faces:
         x1 = face.left()
@@ -51,18 +56,30 @@ while True:
         landmarks = face_utils.shape_to_np(landmarks)
 
         left_blink = blinked(landmarks[36], landmarks[37],
-                            landmarks[38], landmarks[41], landmarks[40], landmarks[39])
+                             landmarks[38], landmarks[41], landmarks[40], landmarks[39])
         right_blink = blinked(landmarks[42], landmarks[43],
-                            landmarks[44], landmarks[47], landmarks[46], landmarks[45])
+                              landmarks[44], landmarks[47], landmarks[46], landmarks[45])
 
-        if left_blink == 0 or right_blink == 0:
+        mouth_open = compute(landmarks[50], landmarks[58]) # Distance between mouth corners
+
+        if mouth_open > 25.0:
+            yawn += 1
+            if yawn > 10:
+                status = "Yawning :("
+                color = (0, 0, 255)
+        else:
+            yawn = 0
+
+        if left_blink == 0 or right_blink == 0 or yawn > 10:
             sleep += 1
             drowsy = 0
             active = 0
             if sleep > 6:
                 status = "SLEEPING !!!"
                 color = (255, 0, 0)
-
+                if not alarm_playing:
+                    pygame.mixer.Channel(0).play(alarm_sound)
+                    alarm_playing = True
         elif left_blink == 1 or right_blink == 1:
             sleep = 0
             active = 0
@@ -70,7 +87,6 @@ while True:
             if drowsy > 6:
                 status = "Drowsy !"
                 color = (0, 0, 255)
-
         else:
             drowsy = 0
             sleep = 0
@@ -78,6 +94,9 @@ while True:
             if active > 6:
                 status = "Active :)"
                 color = (0, 255, 0)
+                if alarm_playing:
+                    pygame.mixer.Channel(0).stop()
+                    alarm_playing = False
 
         cv2.putText(frame, status, (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1.2, color, 3)
 
@@ -90,3 +109,6 @@ while True:
     key = cv2.waitKey(1)
     if key == 27:
         break
+
+cap.release()
+cv2.destroyAllWindows()
